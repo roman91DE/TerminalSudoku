731b806b7dc981a0af207ea91c3eda859728bca7
rmn@mbp <rohoehn123@gmail.com>
2021-10-09 10:02:51 +0200

    auto commit

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# automate src formatting and git 
+
+git pull
+clang-format -style='llvm' -i src/*
+
+git add *
+git commit -m "auto commit"
+git push
+
---
+++ src/board.h
@@ -0,0 +1,3 @@
+#include <array>
+
+class Board {};
