3856549f1001192d6a80bc4c2406c8c53af36682
rmn@mbp <rohoehn123@gmail.com>
2021-10-09 10:20:40 +0200

    auto commit

---
+++ src/board.cpp
@@ -0,0 +1,12 @@
+#include "board.h"
+#include <algorithm>
+#include <fmt/core.h>
+
+Board::Board() {
+  for (auto &row : board)
+    std::fill(row.begin(), row.end(), 0);
+}
+
+Board::~Board() {}
+
+void Board::printBoard const {}
