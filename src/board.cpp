b9f80a1b1abc6d5a3468e4b0865c81e92a8e0936
rmn@mbp <rohoehn123@gmail.com>
2021-10-09 10:27:35 +0200

    auto commit

--- src/board.cpp
+++ src/board.cpp
@@ -13,4 +13,9 @@ Board::~Board() { }
 
 void Board::printBoard const
 {
+    for (auto& row : board) {
+        fmt::print(
+            "|{}{}{}[{}{}{}|{}{}{}|",
+            row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], )
+    }
 }
