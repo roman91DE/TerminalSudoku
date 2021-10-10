f40e4101643d9b9a0f8e1833585674e74edb5cb2
rmn@mbp <rohoehn123@gmail.com>
2021-10-10 17:23:21 +0200

    auto commit

--- src/game.cpp
+++ src/game.cpp
@@ -1,16 +1,16 @@
 #include "game.h"
 
 #include <fmt/core.h>
+
 #include <iostream>
 
 struct Game::Move {
-    uint16_t row, col, val;
-    Move(uint16_t _row, uint16_t _col, uint16_t _val)
-    : row(_row), col(_col), val(_val) {}
-  };
+  uint16_t row, col, val;
+  Move(uint16_t _row, uint16_t _col, uint16_t _val)
+      : row(_row), col(_col), val(_val) {}
+};
 
- enum class Game::Difficulty { easy = 1, medium = 2, hard = 3 };
-
+enum class Game::Difficulty { easy = 1, medium = 2, hard = 3 };
 
 Game::Game(Game::Difficulty difficulty)
     : moveCounter(0),
@@ -33,36 +33,38 @@ void Game::autoSolve() {
   if (sudokuPtr->solve()) {
     sudokuPtr->printSudoku();
   } else {
-    fmt::print("The current state of the game is not solvable!\nStart a new Game? [y/N]\n");
+    fmt::print(
+        "The current state of the game is not solvable!\nStart a new Game? "
+        "[y/N]\n");
     char usrInput;
     std::cin >> usrInput;
     if ((usrInput == 'y') || (usrInput == 'Y'))
       startNewGame(Game::getDifficultyFromPlayer());
     else {
-      finishGame();
+      // finishGame();
+      return;
     }
   }
 }
 
-Game::Difficulty Game::getDifficultyFromPlayer() const {
+Game::Difficulty Game::getDifficultyFromPlayer() {
   fmt::print("Choose Level of Difficulty\n1 - Easy\n2 - Medium\n3 - Hard\n:");
   uint16_t usrInput{42};
   std::cin >> usrInput;
-  switch (usrInput){
+  switch (usrInput) {
     case 1:
-    return Game::Difficulty::easy;
+      return Game::Difficulty::easy;
 
     case 2:
-    return Game::Difficulty::medium;
+      return Game::Difficulty::medium;
 
-    case 3: 
-    return Game::Difficulty::hard;
+    case 3:
+      return Game::Difficulty::hard;
 
     default:
-    fmt::print("{} is not a valid Choice!\n", usrInput);
-    return Game::getDifficultyFromPlayer();
+      fmt::print("{} is not a valid Choice!\n", usrInput);
+      return Game::getDifficultyFromPlayer();
   }
-
 }
 
 void Game::startNewGame(Game::Difficulty difficulty) {
@@ -71,11 +73,10 @@ void Game::startNewGame(Game::Difficulty difficulty) {
   sudokuPtr->resetSudoku();
   sudokuPtr->randomInit(getNumCells(difficulty));
   sudokuPtr->printSudoku();
-
 }
 
 uint16_t Game::getNumCells(Game::Difficulty difficulty) {
-    switch (difficulty) {
+  switch (difficulty) {
     case Game::Difficulty::easy:
       return 20;
     case Game::Difficulty::medium:
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <fmt/core.h>
 
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
+
 #include "game.h"
 #include "sudoku.h"
 
 int main(int, char**) {
-  Game game = Game(Game::Difficulty::medium);
-  game.printGameState();
-  game.autoSolve();
+  std::ifstream ifs;
+  std::string buffer;
+  ifs.open("/Users/rmn/code/c/sudoku/src/logo.txt");
+  if (ifs.is_open()) {
+    while (getline(ifs, buffer)) {
+      fmt::print("{}\n", buffer);
+    }
+  }
+  // Game game = Game(Game::getDifficultyFromPlayer());
+  // game.printGameState();
+  // game.autoSolve();
   return 0;
 }
