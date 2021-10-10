f1a40ad74a44757085aa33fc4765706e12101936
rmn@mbp <rohoehn123@gmail.com>
2021-10-10 15:10:19 +0200

    auto commit

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -6,10 +6,16 @@ Sudoku::Move::Move(uint16_t _row, uint16_t _col, uint16_t _val)
     : row(_row), col(_col), val(_val) {}
 
 Sudoku::Sudoku()
-    : moveCounter(0)
-    : boardPtr(new Board()), moveMemoryPtr(new std::list<Sudoku::Move>()) {}
+    : moveCounter(0),
+      boardPtr(new Board()),
+      moveMemoryPtr(new std::list<Sudoku::Move>()) {}
 
-Sudoku::Sudoku(Sudoku::Difficulty difficulty) : moveCounter(0) {
+Sudoku::Sudoku(Sudoku::Difficulty difficulty)
+    : : moveCounter(0),
+        boardPtr(new Board()),
+        moveMemoryPtr(new std::list<Sudoku::Move>())
+
+{
   uint16_t numCells{10};
 
   switch (difficulty) {
